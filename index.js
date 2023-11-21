b8459cda0ec01c3da07ab87e2a78e439c406cc11
JC <113685133+Jeyasheelanjc@users.noreply.github.com>
2023-11-21 11:42:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function find(){
+    var number =prompt("Eneter the number")
+if(number%2==0){
+    alert("Its is a Even number")
+    // document.write("even number")
+}
+else{
+    alert("Its is an odd number")
+}
+}
+find()
